31bd1ce5d7f559a6684a372210066f6dfc7576ff
victorboi11 <725304@student.dusd.net>
2024-04-16 11:43:20 -0700

    i did some stuff
    nice

--- Unit3SoundAndEffectEstradaP3/Assets/Scripts/PlayerController.cs
+++ Unit3SoundAndEffectEstradaP3/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerRb.AddForce(Vector3.up * 1000);
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce (Vector3.up * 10, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
