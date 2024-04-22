8f72b8041d9b7b1f0654d8794e3fee4f9982fa1e
victorboi11 <725304@student.dusd.net>
2024-04-22 12:19:58 -0700

    refrfgrfg
    rfgrrgfrgf

--- Unit3SoundAndEffectEstradaP3/Assets/Scripts/PlayerController.cs
+++ Unit3SoundAndEffectEstradaP3/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -22,11 +25,20 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce (Vector3.up * 10, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+      
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
     }
 }
